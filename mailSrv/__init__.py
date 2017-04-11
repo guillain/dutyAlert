44a8d0ab02b47db2700f0e2f3a2c584de608132c
guillain <guillain@gmail.com>
2017-04-11 06:39:30 +0200

    prepare for 1.1.0

--- mailSrv/__init__.py
+++ mailSrv/__init__.py
@@ -6,18 +6,12 @@
 
 from mail import popSrvMail
 
-MAIL_HOST = 'pop.gmail.com'
-MAIL_USER = 'dutyAlertApp'
-MAIL_PASS = 'C&1CptDutyAlert!'
+MAIL_HOST = ''
+MAIL_USER = ''
+MAIL_PASS = ''
 
 (subject,content) = popSrvMail(MAIL_HOST,MAIL_USER,MAIL_PASS)
 # ToDo: send REST request to the web server
 print "subject: " + subject
 print "content: " + content
-
-# End of App --------------------------------------------------------------------------
-#if __name__ == '__main__':
-#    sess.init_app(app)
-#    app.debug = app.config['DEBUG']
-#    app.run()
 
